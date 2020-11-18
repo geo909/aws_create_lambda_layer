3fd413ed4a555de407cc911f11df0f390cd1bbf4
Georgios Tzanakis <geo909@users.noreply.github.com>
2020-11-18 14:03:37 +0200

    Update create_layer.sh

--- create_layer.sh
+++ create_layer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-LAYERNAME=everypay-type2-notifier-layer
+LAYERNAME=test-layer
 
 export PKG_DIR="python"
 
 mkdir -p ${PKG_DIR}
 
-# Assumes you are in the virtual environmnet you want to recreate
+# Assumes you are in the virtual environment you want to recreate
 pip freeze > requirements.txt
 docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.8 \
     pip install -r requirements.txt --no-deps -t ${PKG_DIR}
@@ -15,10 +15,13 @@ sudo rm -rf ${PKG_DIR}
 rm requirements.txt
 
 # First store to S3 then publish 
-aws s3 cp $LAYERNAME.zip s3://george-home/Lambda/Layers/$LAYERNAME.zip
-aws lambda publish-layer-version --layer-name $LAYERNAME --content S3Bucket=george-home,S3Key=Lambda/Layers/$LAYERNAME.zip --compatible-runtimes python3.8
+#aws s3 cp $LAYERNAME.zip s3://george-home/Lambda/Layers/$LAYERNAME.zip
+#aws lambda publish-layer-version --layer-name $LAYERNAME --content S3Bucket=george-home,S3Key=Lambda/Layers/$LAYERNAME.zip --compatible-runtimes python3.8
 
 # Publish by directly uploading zip file
-#aws lambda publish-layer-version --layer-name $LAYERNAME --zip-file fileb://$LAYERNAME.zip --compatible-runtimes python3.8
+aws lambda publish-layer-version --layer-name $LAYERNAME --zip-file fileb://$LAYERNAME.zip --compatible-runtimes python3.8
+
+# Publish by directly uploading zip file (FerryHopper version)
+aws --profile ferryhopper --region eu-central-1 lambda publish-layer-version --layer-name $LAYERNAME --zip-file fileb://$LAYERNAME.zip --compatible-runtimes python3.8
 
 rm $LAYERNAME.zip
